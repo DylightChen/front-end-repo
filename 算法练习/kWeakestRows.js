1a528be4f36def1934018b2ae2979529b1ea56f4
Dylight <dylan654311222@gmail.com>
2021-08-03 12:39:27 +0800

    1337. 矩阵中战斗力最弱的 K 行

---
+++ 算法练习/kWeakestRows.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: Dylight
+ * @Date: 2021-07-31 00:20:46
+ * @LastEditors: Dylight
+ * @LastEditTime: 2021-08-03 12:38:45
+ * @FilePath: /Algorithm/c.js
+ * @Description: 
+ */
+var kWeakestRows = function(mat, k) {
+    let map = new Map()
+    //利用map 存储数据源的index
+    mat.forEach((element, index)=> {
+        map.set(element, index)
+    })
+    mat.sort();
+    console.log(map);
+    //根据排序后的数组查找map中的值
+    let a = Array.from({ length: k }, (_, i) => {
+        return map.get(mat[i]) 
+       
+    })
+    
+    console.log(a);
+    
+    return  a
+};
+let mat = [[1,1,0,0,0],
+[1,1,1,1,0],
+[1,0,0,0,0],
+[1,1,0,0,0],
+    [1, 1, 1, 1, 1]]
+
+console.log(kWeakestRows(mat,3));
